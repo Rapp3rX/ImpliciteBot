bbc258886d9227ca11a392284e65c6c8a00ba449
Rapp3rX <galosi.kristof@gmail.com>
2017-12-03 20:37:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
-const PREFIX = "pls ";
+const PREFIX = "imp ";
 
 var client = new Discord.Client();
 
@@ -11,66 +11,77 @@ client.on('ready', () => {
 });
 
 client.on('guildMemberAdd', member => {
-    // Send the message to a designated channel on a server:
     const channel = member.guild.channels.find('name', 'bot-log');
-    // Do nothing if the channel wasn't found on this server
     if (!channel) return;
-    // Send the message, mentioning the member
     channel.send(`${member} csatlakozott a szerverhez!`);
   });
+client.on("message", message => {
+    let msg = message.content.toLowerCase();
+    let sender = message.author;
+    let cont = message.content.slice(PREFIX.length).split(" ");
+    let args = cont.slice(1); 
 
-  client.on('guildMemberRemove', member => {
-    // Send the message to a designated channel on a server:
-    const channel = member.guild.channels.find('name', 'bot-log');
-    // Do nothing if the channel wasn't found on this server
-    if (!channel) return;
-    // Send the message, mentioning the member
-    channel.send(`${member} elhagyta a szervert!`);
-  });
-client.on("message", async message => {
-    var sender = message.author;
-    var msg = message.content.toLowerCase();
-    if (sender.id == '386597563379351563'){
-      return;
-    }    
     if (message.author.equals(client.user)) return;
     
-    if (msg.includes('implicitebot')){
-        message.channel.send(`Igen, én vagyok! Esetleg szeretnél tőlem valamit ${sender}?`);      
+    if (msg.startsWith(PREFIX + 'purge')) { 
+        async function purge() {
+            message.delete(); 
+
+            
+            if (!message.member.roles.find("name", "Bot-Access")) {
+                message.channel.send(`${sender}, ehhez \Bot-Access\ role kell!`);
+                return;
+            }
+
+            if (isNaN(args[0])) {
+                message.channel.send(`${sender}, kérlek adj meg értéket!. \nHasználat: ` + PREFIX + `purge <érték>`);
+                return;
+            }
+
+            const fetched = await message.channel.fetchMessages({limit: args[0]}); 
+            sender.send(fetched.size + ' üzenetet találtam, már is törlöm...');
+
+            message.channel.bulkDelete(fetched)
+                .catch(error => message.channel.send(`Hiba: ${error}`));
+
+        }
+
+        purge(); 
+
     }
-    if (msg.includes('tgf') || msg.includes('tagfelvétel')){
-        message.channel.send(`${sender}, jelenleg nincs tagfelvétel! Ha lesz, azt kihírdetjük a Facebook oldalunkon, vagy a szerveren!`);      
+    if (msg.startsWith(PREFIX + 'készítőd')){
+        message.channel.send("Engem Rapp3rX készített!");
     }
-    if (msg.includes('csá') || msg.includes('szia') || msg.includes('cső') || msg.includes('szevasz')){
-        message.channel.send(`Szia, ${sender}!`);      
+    if (msg.startsWith(PREFIX + 'szerver')){
+        message.channel.send("Hamarosan...");
     }
-    if (msg.includes('anyád') || msg.includes('geci') || msg.includes('büdös') || msg.includes('kurva') || msg.includes('meleg') || msg.includes('fasz') || msg.includes('köcsög') || msg.includes('buzi') || msg.includes('fuck') || msg.includes('homár') ){
+    if (msg.startsWith(PREFIX + 'parancsok')){
+        message.channel.send("Üdvözöllek!\nPrefix: "+ PREFIX + "\nElérhető parancsok tőlem:\n- készítőd(Ki készített)\n- szerver(A szerverről)\n- parancsok(Parancs lista)");
+    }
+
+    if (msg.includes('anyád') || msg.includes('geci') || msg.includes('büdös') || msg.includes('kurva') || msg.includes('meleg') || msg.includes('fasz') || msg.includes('köcsög') || msg.includes('buzi') || msg.includes('fuck') || msg.includes('homár') || msg.includes('4ny4d')  || msg.includes('homo')  || msg.includes('cigány')){
         message.delete();
         message.channel.send(`Töröltem ${sender} üzenetét! Indok: Káromkodás`);      
     }
-});
-client.on("message", function(message) {
-    if (message.author.equals(client.user)) return;
+    else if (msg.includes("http://") || msg.includes("https://") ){
+        if (message.channel.id == '386610409563619328'){
+            console.log('Itt hírdettek, de nem tötént semmi.')
+        } else {
+            message.delete();
+            message.channel.send(`Töröltem ${sender} üzenetét! Indok: Link`); 
+        }
 
-    if (!message.content.startsWith(PREFIX)) return;
-
-    var args = message.content.substring(PREFIX.length).split(" ");
-
-    switch (args[0].toLowerCase()){
-        case "készítőd":
-            message.channel.send("Engem Rapp3rX készített!");
-            break;
-        case "szerver":
-            message.channel.send("Legjobb szerver! (Legalább is belém írták :P)");
-            break;
-        case "parancsok":
-           message.channel.send("Elérhető parancsok tőlem: készítőd, szerver, parancsok");
-           break;
-        default:
-            message.channel.send("Nem tudod mit akarsz? Akkor kérdezd meg tőlem a parancsokat!");
-                        
     }
-  
-});
+    else if (msg.includes('csá') || msg.includes('szia') || msg.includes('cső') || msg.includes('szevasz')){
+        message.channel.send(`Szia, ${sender}!`);      
+    }
+    else if (msg.includes('tgf') || msg.includes('tagfelvétel')){
+        message.channel.send(`${sender}, jelenleg nincs tagfelvétel! Ha lesz, azt kihírdetjük a Facebook oldalunkon, vagy a szerveren!`);      
+    }
+    else if (msg.includes('implicitebot')){
+        message.channel.send(`Igen, én vagyok! Esetleg szeretnél tőlem valamit ${sender}? Mert akkor: pls parancsok`);      
+    }
 
+
+});
 client.login(process.env.BOT_TOKEN);       
