724f0db7116595a3aea1a64402ac687742da5662
Gálosi Kristóf <galosi.kristof@gmail.com>
2019-07-04 19:17:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,35 +3,15 @@ const YTDL = require("ytdl-core");
 
 const PREFIX = "imp ";
 
-var request = require('request');
-var mcIP = 'play.implicite.us';
-var mcPort = 25565;
-
 var client = new Discord.Client();
-var servers = {};
-
-function play(connection, message) {
-    var server = servers[message.guild.id];
-    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
-    server.queue.shift();
-    server.dispatcher.on("end", function(){
-        if (server.queue[0]) play(connection, message);
-        else connection.disconnect();
-    });
-}
 
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setGame("on play.implicite.us");
+  client.user.setGame("on play.implicite.hu");
   client.user.setUsername("ImpliciteBot");
 });
 
-client.on('guildMemberAdd', member => {
-    const channel = member.guild.channels.find('name', 'bot-log');
-    if (!channel) return;
-    channel.send(`${member} csatlakozott a szerverhez!`);
-  });
 client.on("message", message => {
     let msg = message.content.toLowerCase();
     let sender = message.author;
@@ -41,85 +21,8 @@ client.on("message", message => {
 
     if (message.author.equals(client.user)) return;
     
-    if (msg.startsWith(PREFIX + 'purge')) { 
-        async function purge() {
-            message.delete(); 
-
-            
-            if (!message.member.roles.find("name", "Bot-Access")) {
-                message.channel.send(`${sender}, ehhez \Bot-Access\ role kell!`);
-                return;
-            }
-
-            if (isNaN(args[0])) {
-                message.channel.send(`${sender}, kérlek adj meg értéket!. \nHasználat: ` + PREFIX + `purge <érték>`);
-                return;
-            }
-
-            const fetched = await message.channel.fetchMessages({limit: args[0]}); 
-            sender.send(fetched.size + ' üzenetet találtam, már is törlöm...');
-
-            message.channel.bulkDelete(fetched)
-                .catch(error => message.channel.send(`Hiba: ${error}`));
-
-        }
-
-        purge(); 
-
-    }
-    if (msg.startsWith(PREFIX + 'play')) {
-        if (!message.member.roles.find("name", "Bot-Access")) {
-            message.channel.send(`${sender}, ehhez \Bot-Access\ role kell!`);
-            return;
-        }
-        if (!args1[0]){
-            message.channel.send(`${sender}, kérlek adj meg egy linket!`);
-            return;
-        }
-        if (!message.member.voiceChannel){
-            message.channel.send(`Nem egy szobában vagy!`);
-        }
-        if (!servers[message.guild.id]) servers[message.guild.id] = {
-            queue: []
-        };
-
-        var server = servers[message.guild.id];
-        server.queue.push(args1[1]);
-
-        if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function (connection){
-                //server.dispatcher = connection.playStream(YTDL(args1[1], {filter: "audioonly"}));
-                //message.channel.send("Muzsika: " + args1[1]);
-
-            play(connection, message);
-        });
-    }
-    if (msg.startsWith(PREFIX + 'skip')){
-        var server = servers[message.guild.id];
-        if (server.dispatcher) server.dispatcher.end();
-    }
-    if (msg.startsWith(PREFIX + 'stop')){
-        var server = servers[message.guild.id];
-        if (!message.member.roles.find("name", "Bot-Access")) {
-            message.channel.send(`${sender}, ehhez \Bot-Access\ role kell!`);
-            return;
-        } 
-        if (message.guild.voiceConnection)
-        {
-            for (var i = server.queue.length - 1; i >= 0; i--) 
-            {
-                server.queue.splice(i, 1);
-         }
-            server.dispatcher.end();
-            console.log("[" + new Date().toLocaleString() + "] Stopped the queue.");
-            message.guild.voiceConnection.disconnect();
-            message.channel.send("Sikeresen leállítottad a zenét!");
-        }
-
-    }
-    if (msg.startsWith(PREFIX + 'készítőd')){
-        message.channel.send("Engem Rapp3rX készített!");
-    }
-    if (msg.startsWith(PREFIX + 'szerver')){
+    
+    /*if (msg.startsWith(PREFIX + 'szerver')){
         var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
         request(url, function(err, response, body) {
             if(err) {
@@ -149,7 +52,7 @@ client.on("message", message => {
     if (msg.includes('anyád') || msg.includes('geci') || msg.includes('büdös') || msg.includes('kurva') || msg.includes('meleg') || msg.includes('fasz') || msg.includes('köcsög') || msg.includes('buzi') || msg.includes('fuck') || msg.includes('homár') || msg.includes('4ny4d')  || msg.includes('homo')  || msg.includes('cigány')){
         message.delete();
         message.channel.send(`Töröltem ${sender} üzenetét! Indok: Káromkodás`);      
-    }
+    }*/
     /*else if (msg.includes("http://") || msg.includes("https://") ){
         if (message.channel.id == '386610409563619328'){
             console.log('Itt hírdettek, de nem tötént semmi.')
@@ -159,11 +62,8 @@ client.on("message", message => {
         }
 
     }*/
-    else if (msg.includes('tgf') || msg.includes('tagfelvétel')){
-        message.channel.send(`${sender}, jelenleg nincs tagfelvétel! Ha lesz, azt kihírdetjük a Facebook oldalunkon, vagy a szerveren!`);      
-    }
-    else if (msg.includes('implicitebot')){
-        message.channel.send(`Igen, én vagyok! Esetleg szeretnél tőlem valamit ${sender}? Mert akkor: imp parancsok`);      
+    if (msg.startsWith('-new') && client.channels.find("id","481138002064834589")) {
+         message.delete();
     }
 
 
